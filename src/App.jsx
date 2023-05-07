de87ed33ae81520c845e4e62766225d8263d4ff6
Abdullah Al MahMud <abdudevs@gmail.com>
2023-05-07 16:13:49 +0600

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>lalalallalalla</h2>
+    </>
+  );
+}
+
+export default App;
